f509ff5d4ded705a6d878aa214e1d7ca7d268dfb
adcavalcant <aderc19@gmail.com>
2023-06-27 10:11:17 -0300

    Operadores de Comparação

---
+++ comparacoes.js
@@ -0,0 +1,13 @@
+// == (comparação implícita)
+
+let numero = 1;
+let texto = "1";
+
+console.log(numero == texto);
+
+// === (comparação explícita)
+
+let numero2 = 1;
+let texto2 = "1";
+
+console.log(numero2 === texto2);
