cb4ebda4a3656829251d9c742427a4b3bc8c4531
adcavalcant <aderc19@gmail.com>
2023-06-27 10:16:06 -0300

    typeof e conversões explícitas

--- comparacoes.js
+++ comparacoes.js
@@ -1,13 +1,21 @@
-// == (comparação implícita)
+// == (comparação implícita) - só compara o valor
 
 let numero = 1;
 let texto = "1";
 
 console.log(numero == texto);
 
-// === (comparação explícita)
+// === (comparação explícita) - compara o valor e o tipo de dado (boa prática)
 
 let numero2 = 1;
 let texto2 = "1";
 
 console.log(numero2 === texto2);
+
+//typeof
+console.log(typeof numero);
+console.log(typeof texto);
+
+//conversão explícita
+Number();
+String();
